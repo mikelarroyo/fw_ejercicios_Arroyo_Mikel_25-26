79f4a4b3b47ac830488981e4e7399f3a2f95a790
Mikel <mikelarroyogomez@gmail.com>
2026-05-27 12:48:49 +0200

    Frontend: actualizar URL API a Railway

--- fw_t04p01_arroyo_mikel/front_ejercicio1_arroyo/src/app/services/dragon-ball.ts
+++ fw_t04p01_arroyo_mikel/front_ejercicio1_arroyo/src/app/services/dragon-ball.ts
@@ -6,8 +6,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class DragonBall {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/api';
-  private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNmExMDNjNmVhMTkyNDcyZDg4NWUzMGMzIiwiaWF0IjoxNzc5ODY5NzUzLCJleHAiOjE3ODA0NzQ1NTN9.qyQ0XNbwN8WY_QWFR52l8Zl3oRkfk4n8Ig6ePPiZg4s';
+  private apiUrl = 'https://dragonball-api-production.up.railway.app/api';
+  private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNmExMDNjNmVhMTkyNDcyZDg4NWUzMGMzIiwiaWF0IjoxNzc5ODc3ODM4LCJleHAiOjE3ODI0Njk4Mzh9.RrQUOsmPM4FXVS6F4N0qENSUmGVAY_xy73mp84y1-jE';
 
   private headers = new HttpHeaders({
     'Authorization': `Bearer ${this.token}`
