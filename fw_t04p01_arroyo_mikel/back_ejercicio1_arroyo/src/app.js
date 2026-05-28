4382869d0c677fd0772dd1a09c779190d93b9821
Mikel <mikelarroyogomez@gmail.com>
2026-05-28 08:41:43 +0200

    Actualizar CORS backend y netlify.toml frontend

--- fw_t04p01_arroyo_mikel/back_ejercicio1_arroyo/src/app.js
+++ fw_t04p01_arroyo_mikel/back_ejercicio1_arroyo/src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(cors({ origin: ['http://localhost:4200', 'https://6a17303f90c2d28421d89554--eloquent-cuchufli-7e98e2.netlify.app'] }));
 app.use(express.json());
 
 app.use('/api', require('./routes/api'));
