37c0543c7a197741eaacd5354325e5a48fea02d5
Mikel <mikelarroyogomez@gmail.com>
2026-06-08 16:58:45 +0200

    Actualizacion

--- fw_t02p02_Arroyo_Mikel/ejercicio3_meals_arroyo/src/app/c_pages/plan-week/plan-week.ts
+++ fw_t02p02_Arroyo_Mikel/ejercicio3_meals_arroyo/src/app/c_pages/plan-week/plan-week.ts
@@ -1,7 +1,11 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { AuthService } from './../../services/auth-service';
+import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlanWeekCreate } from '../plan-week-create/plan-week-create';
 import { PlanWeekList } from '../plan-week-list/plan-week-list';
+import { LocalStorageService } from '../../services/local-storage-service';
+import { ApiService } from '../../services/api-service';
+import { IWeeklyPlan } from '../../model/i-weekly-plan';
 
 
 @Component({
@@ -11,4 +15,13 @@ import { PlanWeekList } from '../plan-week-list/plan-week-list';
   templateUrl: './plan-week.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlanWeek {}
+export class PlanWeek {
+  private auth = inject(AuthService);
+  private localStorage = inject(LocalStorageService);
+  private api = inject(ApiService);
+
+
+  
+  //Estado
+  plan = signal<IWeeklyPlan | null>(null);
+}
